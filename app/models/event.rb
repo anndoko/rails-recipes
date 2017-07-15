6a811f01b990628a74caacc416a335c76462900c
Yu-An Ko <handcuffanndo@gmail.com>
2017-07-15 14:39:32 +0800

    implement event category

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
 
+  belongs_to :category, :optional => true
+
   validates_presence_of :name, :friendly_id
   validates_uniqueness_of :friendly_id
   validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
