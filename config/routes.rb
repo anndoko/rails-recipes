acac4452241fbcbb9195528f892599ed1610451d
Yu-An Ko <handcuffanndo@gmail.com>
2017-07-15 15:14:27 +0800

    implement admin::users

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,13 @@ Rails.application.routes.draw do
 
   devise_for :users
   resource :user
-  
+
   resources :events
 
   namespace :admin do
     root "events#index"
     resources :events
+    resources :users
   end
 
   root "events#index"
